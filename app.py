84de1eb1df024bb813be003e4b7fe38348fcf37d
LucaKao <lars10192002@gmail.com>
2021-01-26 18:06:57 +0800

    [ch3]add errorhandler(404,500)

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
@@ -13,7 +24,3 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
